a17040bec7835c177cf08143c45506457433484e
neecholaus <thisisnickadam@gmail.com>
2024-11-30 19:37:32 -0800

    feat: scaffolding for basic account endpoints

--- api/routing.go
+++ api/routing.go
@@ -4,8 +4,19 @@ import "net/http"
 
 func registerRoutes(h *http.ServeMux) {
 	h.HandleFunc("GET /ping", ping)
+
+	h.HandleFunc("POST /account/make-session", makeSession)
+	h.HandleFunc("POST /account/create", createAccount)
 }
 
 func ping(w http.ResponseWriter, r *http.Request) {
-	w.Write([]byte("pong\n"))
+	w.Write([]byte("pong"))
+}
+
+func createAccount(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("not implemented"))
+}
+
+func makeSession(w http.ResponseWriter, r *http.Request) {
+	w.Write([]byte("not implemented"))
 }
